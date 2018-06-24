b4ee602887e0536be42bd4c6e3b50f4f13233219
shibuya takumi <takm.reason@gmail.com>
2018-06-25 03:46:13 +0900

    shift routes追加

---
+++ express/src/shift/components/CalendarForm.jsx
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import {observable, action} from 'mobx';
+import {observer} from 'mobx-react';
+
+import Month from './Month';
+
+@observer
+class CalendarForm extends React.Component {
+  render() {
+    return (
+      <form action="/" method="POST">
+        <table className="table" style={{tableLayout: 'fixed'}}>
+          <thead>
+            <tr>
+              <th
+                className="text-center"
+                colSpan="2"
+                onClick={this.props.store.prevMonth}
+              >&lt;&lt;prev</th>
+              <th
+                className="text-center"
+                colSpan="3"
+              >
+                {this.props.store.date.getFullYear()
+                }年 {this.props.store.date.getMonth() + 1}月
+              </th>
+              <th
+                className="text-center"
+                colSpan="2"
+                onClick={this.props.store.nextMonth}
+              >next&gt;&gt;</th>
+            </tr>
+            <tr>
+              <th className="text-center">月</th>
+              <th className="text-center">火</th>
+              <th className="text-center">水</th>
+              <th className="text-center">木</th>
+              <th className="text-center">金</th>
+              <th className="text-center">土</th>
+              <th className="text-center">日</th>
+            </tr>
+          </thead>
+          <Month month={this.props.store.month}/>
+        </table>
+        <div className="text-right">
+          <button
+            className="btn btn-success"
+            type="submit">
+            Save
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default CalendarForm;
