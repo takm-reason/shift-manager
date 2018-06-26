8bb70fbeb3bae016fa3df9d60984d1185948abd0
shibuya takumi <takm.reason@gmail.com>
2018-06-26 20:05:11 +0900

    webpackのentryを自動で取得するように変更

--- express/webpack.config.js
+++ express/webpack.config.js
@@ -1,12 +1,30 @@
 const path = require('path');
 const webpack = require('webpack');
+const fs = require('fs');
+
+const folderlist = () => {
+  return fs.readdirSync('./src', (err, list) =>{
+    if (err) {
+    } else {
+      return list;
+    }
+  });
+};
+
+const entry = (list) => {
+  list = list.filter((folder) => {
+    return !(folder[0] == '.');
+  });
+  list = list.reduce((accumulator, currentValue) => {
+    accumulator[`${currentValue}`] = `./src/${currentValue}/index`;
+    return accumulator;
+  }, {});
+  return list;
+};
 
 module.exports = {
   devtool: 'eval',
-  entry: {
-    calendarform: './src/calendarform/index',
-    shift: './src/shift/index',
-  },
+  entry: entry(folderlist()),
   output: {
     path: path.join(__dirname, 'public/dist'),
     filename: '[name].js',
