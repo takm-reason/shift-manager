0b8e9346ebf39dcc9ad0aa1ba02257736d8ad310
shibuya takumi <takm.reason@gmail.com>
2018-06-29 01:51:18 +0900

    database/usersへアクセスできるよう追加

--- express/routes/database.js
+++ express/routes/database.js
@@ -12,4 +12,19 @@ router.get('/', (req, res, next) => {
   });
 });
 
+/* GET home page. */
+router.get('/:table', (req, res, next) => {
+  getModel().table(req.params.table, (err, results) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.render('database', {
+      title: 'Express',
+      tablename: req.params.table,
+      tabledate: results,
+    });
+  });
+});
+
 module.exports = router;
