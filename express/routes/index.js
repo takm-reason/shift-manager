81d98345a678486343e315a94281578fa2f62967
shibuya takumi <takm.reason@gmail.com>
2018-07-12 22:16:00 +0900

    index.pugを編集

--- express/routes/index.js
+++ express/routes/index.js
@@ -7,10 +7,46 @@ const getModel = () => {
   return require(`./model-mysql`);
 };
 
+const yesterday = (date) => {
+  return new Date(date.setDate(date.getDate() - 1));
+};
+
+const prevMonday = (date) => {
+  if (date.getDay() == 1) {
+    return date;
+  }
+  return prevMonday(yesterday(date));
+};
+
+const nextDate = (date, nextdate = 1) => {
+  return new Date(date.setDate(date.getDate() + nextdate));
+};
+
+const addNextDate = (date, lastDay) => {
+  if (date.length >= 7) {
+    return date;
+  }
+  return addNextDate(
+    date.concat(nextDate(new Date(date[date.length - 1]))),
+    lastDay
+  );
+};
+
+const week = (date) => {
+  return addNextDate([date], nextDate(new Date(date)), 7);
+};
+
+const nextWeek = (date) => {
+  return new Date(date.setDate(date.getDate + 7));
+};
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
+  console.log(week(prevMonday(new Date())).map((date) => {
+    return date.getDate();
+  }));
   async.parallel({
-    plans: (callback) => {
+    user: (callback) => {
       getModel().read(`plans`, `userid`, req.user.id,
       (err, results) => {
         if (err) {
@@ -28,17 +64,21 @@ router.get('/', (req, res, next) => {
       req: req,
       title: view.title,
       nav: view.nav,
-      side: [
-        {
-          text: 'index.get',
-          href: './shift',
-        },
-      ],
+      side: [],
       main: {
         type: 'table',
         title: '休み希望を入力するカレンダー',
         id: 'calendarform',
         results: results.plans,
+        week1: week(prevMonday(new Date())).map((date) => {
+          return date.getDate();
+        }),
+        week2: week(prevMonday(nextWeek(new Date()))).map((date) => {
+          return date.getDate();
+        }),
+        week3: week(prevMonday(nextWeek(nextWeek(new Date())))).map((date) => {
+          return date.getDate();
+        }),
       },
     });
   });
