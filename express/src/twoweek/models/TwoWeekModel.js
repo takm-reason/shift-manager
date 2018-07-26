dd5e4070d478cea6d1568b1135d1d923c5f6f72f
shibuya takumi <takm.reason@gmail.com>
2018-07-26 23:38:02 +0900

    calendarformから転記した箇所で使用していない箇所を削除

--- express/src/twoweek/models/TwoWeekModel.js
+++ express/src/twoweek/models/TwoWeekModel.js
@@ -7,29 +7,9 @@ const yesterday = (date) => {
   return new Date(date.setDate(date.getDate() - 1));
 };
 
-// 引数の月の最初の日を返す
-const firstDate = (date, int = 1) => {
-  return new Date(date.setDate(int));
-};
-
-// 引数の月の最後の日を返す
-const lastDate = (date) => {
-  return yesterday(nextMonth(firstDate(date)));
-};
-
 // 引数の指定日数後のDateを返す
 const nextDate = (date, nextdate = 1) => {
   return new Date(date.setDate(date.getDate() + nextdate));
-};
-
-// 引数の1ヶ月後を返す
-const nextMonth = (date) => {
-  return new Date(date.setMonth(date.getMonth() + 1));
-};
-
-// 引数の1年後を返す
-const nextYear = (date) => {
-  return new Date(date.setFullYear(date.getFullYear() + 1));
 };
 
 // 引数の前月曜日を返す
@@ -40,28 +20,7 @@ const prevMonday = (date) => {
   return prevMonday(yesterday(date));
 };
 
-// 引数の月の最初の日の前月曜日を返す
-const firstMonday = (date) => {
-  return prevMonday(firstDate(new Date(date)));
-};
-
-// 引数の月の最後の日の前月曜日を返す
-const lastMonday = (date) => {
-  return prevMonday(lastDate(new Date(date)));
-};
-
-// 引数1の配列に最後の要素の1週間後を追加して返す
-const addNextDay = (date, lastDay) => {
-  if (date[date.length - 1].getTime() >= lastDay.getTime()) {
-    return date;
-  }
-  return addNextDay(
-    date.concat(nextDate(new Date(date[date.length - 1]), 7)),
-    lastDay
-  );
-};
-
-// 引数の日のfirstMondayから一年後のlastMondayofthemonthまでの7日ごとの配列を返す
+// 引数の日の前月曜日とその次の月曜日を返す
 const mondays = (date) => {
   date = [prevMonday(date)].concat(nextDate(new Date(prevMonday(date)), 7));
   return date;
