542647c3c081e71372f223d812217333e120aefd
shibuya takumi <takm.reason@gmail.com>
2018-07-31 23:50:37 +0900

    twoweek component 修正

--- express/src/twoweek/components/Date.jsx
+++ express/src/twoweek/components/Date.jsx
@@ -2,19 +2,15 @@ import React, {Component} from 'react';
 import {observer} from 'mobx-react';
 
 const Date = observer(({date}) => (
-  <td className="text-muted p-1">
-    <div>
-      <p className="m-0" style={{float: 'left'}}>{date.text}</p>
+  <td className="text-muted p-1">{date.text}
       {
         date.yasumi
-        ? <p className="m-0 text-right" style={{
-          backgroundColor: '#ffff00',
-        }}>休</p>
-        : <p className="m-0 text-right" style={{visibility: 'hidden'}}>休</p>
+        ? <div className="text-center">休</div>
+        : <div>
+            <div className="text-center">17:00</div>
+            <div className="text-center">22:00</div>
+          </div>
       }
-    </div>
-    <div className="text-center">17:00</div>
-    <div className="text-center">22:00</div>
   </td>
 ));
 
