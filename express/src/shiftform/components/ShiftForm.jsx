2c16d36b8b2ec32ea22238bb20a5c3cc8fc664e6
shibuya takumi <takm.reason@gmail.com>
2018-08-01 02:25:08 +0900

    thにmin-width設定

--- express/src/shiftform/components/ShiftForm.jsx
+++ express/src/shiftform/components/ShiftForm.jsx
@@ -10,33 +10,50 @@ class ShiftForm extends React.Component {
     return (
       <form action="/" method="POST">
         <div className="table-responsive">
-          <table className="table table-bordered" style={{
-            tableLayout: 'fixed'}}>
+          <table className="table table-bordered">
             <thead>
               <tr>
                 <th
                   className="text-center"
                   colSpan="3"
+                  style={{
+                    minWidth: '75px',
+                  }}
                 ></th>
                 <th
                   className="text-center"
                   colSpan="2"
+                  style={{
+                    minWidth: '50px',
+                  }}
                 >-8</th>
                 <th
                   className="text-center"
                   colSpan="5"
+                  style={{
+                    minWidth: '125px',
+                  }}
                 >8~13</th>
                 <th
                   className="text-center"
                   colSpan="4"
+                  style={{
+                    minWidth: '100px',
+                  }}
                 >13~17</th>
                 <th
                   className="text-center"
                   colSpan="5"
+                  style={{
+                    minWidth: '125px',
+                  }}
                 >17~22</th>
                 <th
                   className="text-center"
                   colSpan="2"
+                  style={{
+                    minWidth: '50px',
+                  }}
                 >22-</th>
               </tr>
             </thead>
